c7240f3fc86ab07ea09914330b00cd3151f288be
Hafsa <hafsa.abdikadir@student.moringaschool.com>
2025-03-15 04:11:53 +0300

    functions-continued

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+
+// Function declaration for saturdayFun
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Function expression for mondayWork
+  const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  // Function that returns another function for wrapping adjectives
+  function wrapAdjective(flair = "*") {
+    return function(adjective) {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
